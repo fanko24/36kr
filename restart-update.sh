0d0acb11a035268f03243df11cdc224ae7be71d1
刘其文 <liuqiwen@liuqiwens-MacBook-Pro.local>
2019-05-14 18:26:21 +0800

    rm

--- restart-update.sh
+++
@@ -1,5 +0,0 @@
-ts=`date +"%Y-%m-%d %H:%M:%S"`
-echo "restart $ts" >> update.log
-sh stop-update.sh
-sleep 2s
-sh start-update.sh
--- stop-history.sh
+++
@@ -1,7 +0,0 @@
-ts=`date +"%Y-%m-%d %H:%M:%S"`
-echo "stop $ts" >> history.log
-ids=`ps -ef | grep "history.py" | grep -v "$0" | grep -v "grep" | awk '{print $2}'`
-for id in $ids
-do
-kill -9 $id
-done
